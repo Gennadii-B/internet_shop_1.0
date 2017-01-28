b5259871ad2f530400b5e8930a8826f3250026ae
Gennadii-B <manunification@icloud.com>
2017-01-28 12:34:16 +0300

    корзина сохраняется в бд postgreSQL__--

--- src/ru/on_the_zero/Main.java
+++ src/ru/on_the_zero/Main.java
@@ -44,7 +44,7 @@ public class Main {
                 System.out.println("упс =)");
             }
         }
-///
+/////
         packing.packBasketDB(shop.getBasket());
         packing.pack();
         daoBaseController.close();
